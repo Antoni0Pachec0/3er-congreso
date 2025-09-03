42705943460ed977da73b40f478861f8930cbcef
PonchoSaldana <saldanaalfonso68@gmail.com>
2025-09-02 18:45:47 -0600

    ..

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PaymentsModule } from './payments/payments.module';
+import { UserModule } from './user/user.module';
+import { PaymentModule } from './payment/payment.module';
+import { ScheduleModule } from './schedule/schedule.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PaymentsModule, UserModule, PaymentModule, ScheduleModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,3 +7,13 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+=======
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3001);
+}
+bootstrap();
+>>>>>>> 712cd478ccdc3280a208c4a7de01e261193e8481
