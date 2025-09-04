d9b928d668635df24d3f92d2dec64a3cf2cf30ad
PonchoSaldana <saldanaalfonso68@gmail.com>
2025-09-04 09:58:31 -0600

    ..

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
-=======
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
-}
-bootstrap();
->>>>>>> 712cd478ccdc3280a208c4a7de01e261193e8481
-=======
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
@@ -39,4 +18,3 @@ async function bootstrap() {
   logger.log(`Application is running on: ${envs.port}`);
 }
 bootstrap();
->>>>>>> asp
