1da0857a6162909012eaa47dbc3ae869a73dae1d
EliasibContre <cesareliasib018@gmail.com>
2025-09-05 14:59:28 -0600

    swagger added

--- src/payment/dto/create-payment-intent.dto.ts
+++ src/payment/dto/create-payment-intent.dto.ts
@@ -1,5 +1,6 @@
 import { IsString, IsIn, IsInt, IsOptional, IsArray, Min, ValidateNested} from "class-validator";
 import { Type } from "class-transformer";
+import { ApiProperty } from "@nestjs/swagger";
 
 // DTO for individual payment items in the payment intent request payload to validate structure and types.
 export class PaymentItemDto {
@@ -11,10 +12,9 @@ export class PaymentItemDto {
 
 // DTO for creating a payment intent request payload to validate structure and types.
 export class CreatePaymentIntentDto {
+    @ApiProperty({description: 'Order ID as a string representing a bigint', example: '123456789012345678'})
     @IsString() orderId!: string;
-    @IsString() @IsIn(['MXN', 'USD']) currency!: 'MXN' | 'USD';
+    @IsString() @IsIn(['MXN']) currency!: 'MXN' ;
     @IsArray() @ValidateNested({each: true}) @Type(() => PaymentItemDto) items!: PaymentItemDto[];
     @IsOptional() @IsString() customerEmail?: string;
-
-
 }
--- src/payment/dto/update-payment.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePaymentDto } from './create-payment-intent.dto';
-
-export class UpdatePaymentDto extends PartialType(CreatePaymentDto) {}
