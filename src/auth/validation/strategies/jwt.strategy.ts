46b7f07aa300ca46a318f7be7dc85e5499fb4a7e
PonchoSaldana <saldanaalfonso68@gmail.com>
2025-09-26 13:11:42 -0600

    base deploy

--- src/auth/validation/strategies/jwt.strategy.ts
+++ src/auth/validation/strategies/jwt.strategy.ts
@@ -4,11 +4,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
 import { PrismaService } from '@prisma/prisma.service';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-<<<<<<< HEAD:src/auth/strategies/jwt.strategy.ts
-import { envs } from '../../config/envs';
-=======
-import { envs } from '@/config/envs';
->>>>>>> asp:src/auth/validation/strategies/jwt.strategy.ts
+// import { envs } from '../../config/envs';
 
 // En tu archivo jwt.strategy.ts
 // ...
