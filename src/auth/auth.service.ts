0bc8495ae14dc2b30a647b20dfeff7923b469bff
Antonio Pacheco <antonio6pache6@gmail.com>
2025-10-01 13:24:36 -0600

    fixed user 3

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -293,7 +293,7 @@ export class AuthService {
         baseData.matricula = null;
       } else if (typeUserId === 3) {
         // Externo: Solo procedencia y talla
-        baseData.provenance = provenance;
+        baseData.provenance = null;
         baseData.educational_program = null;
         baseData.grade = null;
         baseData.group_user = null;
