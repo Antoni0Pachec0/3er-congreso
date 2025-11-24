84c1f46f8ca4d0285793fee89489f6b8b7bbbdc2
Antoni0Pachec0 <antonio6pache6@gmail.com>
2025-11-23 23:16:10 -0600

    Lector QR

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,14 +1,19 @@
 // src/admin/admin.module.ts
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '@prisma/prisma.module';
+
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+
+import { AttendanceController } from './attendance.controller';
+import { AttendanceService } from './attendance.service';
+
 import { RolesGuard } from '@/common/guards/roles.guard';
-import { PrismaModule } from '@prisma/prisma.module'; // 👈 importa el módulo
 
 @Module({
-  imports: [PrismaModule],                 // 👈 agrega PrismaModule
-  controllers: [AdminController],
-  providers: [AdminService, RolesGuard],   // 👈 quita PrismaService
+  imports: [PrismaModule],
+  controllers: [AdminController, AttendanceController],
+  providers: [AdminService, AttendanceService, RolesGuard],
   exports: [AdminService],
 })
 export class AdminModule {}
